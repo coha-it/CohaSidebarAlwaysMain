257729da3cc655e68c65fb3909eedbe11450e415
Corporate Happiness <it@corporate-happiness.de>
2019-04-30 10:53:55 +0200

    Sidebar Always Main

---
+++ CohaSidebarAlwaysMain.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace CohaSidebarAlwaysMain;
+
+use Shopware\Components\Plugin;
+
+class CohaSidebarAlwaysMain extends Plugin
+{
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PreDispatch_Frontend' => ['onFrontend',-100],
+            'Enlight_Controller_Action_PreDispatch_Widgets' => ['onFrontend',-100]
+        ];
+    }
+
+    /**
+     * @param \Enlight_Event_EventArgs $args
+     * @throws \Exception
+     */
+    public function onFrontend(\Enlight_Event_EventArgs $args)
+    {
+        $this->container->get('Template')->addTemplateDir(
+            $this->getPath() . '/Resources/views/'
+        );
+    }
+
+}
